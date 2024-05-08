320717f6054a088bda8ec51d57815c3ec262616a
Takayuki Uchida <157561926+tisuchida@users.noreply.github.com>
2024-05-08 22:17:26 +0900

    feat: 公開APIの追加 (#8)

--- src/main/java/nablarch/core/transaction/TransactionFactory.java
+++ src/main/java/nablarch/core/transaction/TransactionFactory.java
@@ -1,10 +1,13 @@
 package nablarch.core.transaction;
 
+import nablarch.core.util.annotation.Published;
+
 /**
  * トランザクション制御オブジェクト({@link Transaction})を生成するインタフェース。。
  *
  * @author Hisaaki Sioiri
  */
+@Published(tag = "architect")
 public interface TransactionFactory {
 
     /**
